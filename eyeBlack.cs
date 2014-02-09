5ac5f4152eb6acbb712d125d951d6355a7a376c6
Jack Menhorn <jack.menhorn@gmail.com>
2014-02-08 21:34:54 -0500

    Added Black screen toggle
    Added script to a full screen blackout to represent the players eyes
    closed
    
    Listening to Chtonic

---
+++ eyeBlack.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class eyeBlack : MonoBehaviour
+{
+			
+
+
+
+	void Awake()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		// If the fire button is pressed...
+		if(Input.GetButtonDown("Fire1"))
+			{
+			Debug.Log("Black was turned off");
+			
+			renderer.enabled = false;
+			}
+		if(Input.GetButtonUp("Fire1"))
+			{
+				
+			renderer.enabled = true;
+			//audio.Play();
+			}
+	}
+	}
